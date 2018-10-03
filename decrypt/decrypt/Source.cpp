0ef3fed529e5c2e5f11363820a6320736963ee4a
CurtLiao <vermouth0226@gmail.com>
2018-10-03 19:51:53 +0800

    revise product_cipher

--- decrypt/decrypt/Source.cpp
+++ decrypt/decrypt/Source.cpp
@@ -83,28 +83,18 @@ void Playfair_cipher(std::string message, std::string key) {
 			std::cout << std::endl;
 	}
 }
-void Vernam_cipher(std::string message, std::string key) {
-	
-}
-void Row_transposition(std::string message, int *key) {
+void Row_transposition(std::string message, int key[]) {
 	std::cout << std::left << std::setw(22) << "Row transposition " << " : ";
 	std::cout << message << " ---> ";
 	char temp = ' ';
 	std::string output = "";
-	/*int a[8][2] = {};
-	for (int i = 0; i < 8; i++) {
-		for (int j = 1; j < 2; j++) {
-			if (i >= 4)
-				a[i][j] = 2;
-			else
-				a[i][j] = 3;
-		}
-		a[i][0] = key[i];
-	}*/
+	//int Arr1[8] = { 3, 1, 5, 6, 2, 4, 8, 7 };
+	cout << "array (on stack): ";
+	cout << sizeof(key) / sizeof(key[0]) << endl;
 	for (int i = 0,count = 0; i < message.size(); i+=8) {
-		for (int j = 0; j < 8; j++) {
+		for (int j = 0; j < sizeof(key)/sizeof(key[0]); j++) {
 			count++;
-			if (count > 20)
+			if (count > message.size())
 				break;
 			int index = 0;
 			for (int k = 1; k < key[j]; k++) {
@@ -125,8 +115,8 @@ void Product_cipher(std::string message, int* key) {
 	std::cout << message << " ---> ";
 	int temp =0;
 	char tmp = ' ';
-	for (int i = 0; i < 20; i++) {
-		for (int j = 0; j < 20 - i - 1; j++) {
+	for (int i = 0; i < message.size(); i++) {
+		for (int j = 0; j < message.size() - i - 1; j++) {
 			if (key[j] > key[j + 1])
 			{
 				std::swap(key[j], key[j + 1]);
@@ -160,54 +150,53 @@ string Xor(string a, string b) {
 	}
 	return code;
 }
-void Vernam_ciphers(string text, string key) {
-	cout << left << setw(22) << "Vernam cipher" << ": ";
-	for (int i = 0, j = 0, flag = 0; i < text.length(); ++i) {
-		//cout << (char)((((text[i] - 'A') ^ (key[j] - 'A')) % 26 ) + 'A');
-		char code = char(toint(Xor(todigit(text[i] - 'A'), todigit(key[j] - 'A'))) + 'A');
-		cout << code;
-
-		/*if (flag == 0 && (j + 1 >= key.length())) {
-			flag = 1;
-			j = 0;
-			key = text;
-		}
-		else */
-		j = (j + 1) % key.length();
-	}
-	cout << endl;
-}
 string Upper(string t) {
 	for (int i = 0; i < t.length(); i++) {
 		t[i] = toupper(t[i]);
 	}
 	return t;
 }
+void Vernam_cipher(string text,string key) {
+	for (int i = 0,k=0; i < text.size();i+=3) {
+		for (int j = 0; j < key.size(); j++) {
+			if (k >= text.size())
+				break;
+			char code = char(toint(Xor(todigit(text[k] - 'A'), todigit(key[j] - 'A'))) + 'A');
+			key[j] = code;
+			cout << code;
+			k++;
+		}	
+	}
+}
+
+
+void row(string message, string key) {
+	size_t newSize = key.size();
+	int* newArr = new int[newSize];
+
+}
 int main()
 {
 	string plaintext = "keepgoingnevergiveup", upperplaintext, GGBIG = "IKJNIDKDLPKYG'LK\\JWB";
 	upperplaintext = Upper(plaintext);
-	char ch = ' ';
+	//char ch = ' ';
 	std::string message[6] = { "RLLWNVPUNULCLYNPCLBW", "ATTHUGOFUFTCTKUOCTXH", "MCCROUALFOCXMXDBXCPQ",
-										"IKJFCKHLIFJTJVTMECCA", "IKJNIDKDLPKYG`LK\JWB", "GEUEIEIREPPVGOKGVENN" };
+									"IKJFCKHLIFJTJVTMECCA", "IKJNIDKDLPKYG`LK\JWB", "GEUEIEIREPPVGOKGVENN" };
 
-	const std::string MonoaKey = "MNBVCXZLKJHGFDSAPOIUYTREWQ";
-	int ProductKey[20] = { 15,11,19,18,16,3,7,14,2,20,4,12,9,6,1,5,17,13,10,8 };
+	//const std::string MonoaKey = "MNBVCXZLKJHGFDSAPOIUYTREWQ";
+	//int ProductKey[20] = { 15,11,19,18,16,3,7,14,2,20,4,12,9,6,1,5,17,13,10,8 };
 	int RowKey[8] = { 3, 1, 5, 6, 2, 4, 8, 7 };  //key = 31562487
-	const std::string ke = "CONCONCONCONCONCONCO";
-	const std::string k = "IKJNIDKDLPKYG`LK\JWB";
-	/*Caesar_cipher(message[0]);
-	Monoalphabetic_cipher(message[1], MonoaKey);
-	Playfair_cipher(message[2], "HIT");
-	Vernam_cipher(message[4], "CON");
+	//const std::string ke = "CONCONCONCONCONCONCO";
+	//string plainkey = "CONKEEPGOINGNEVERGIV";
+	//const std::string k = "IKJNIDKDLPKYG`LK\JWB";
+	const std::string newkey = "IKJFCKHLIFJTJVTMEC][";
+	//Caesar_cipher(message[0]);
+	//Monoalphabetic_cipher(message[1], MonoaKey);
+	//Playfair_cipher(message[2], "HIT");
+	//Vernam_cipher(message[4], "CON");
 	Row_transposition(message[4], RowKey);
-	Product_cipher(message[5], ProductKey);*/
-	Vernam_ciphers(upperplaintext, "CON");
-	std::cout << (char)(('H' ^ 'X'))<<std::endl;
-	ch = (char)('H' ^ 'X');
-	for (int i = 0; i < 20; i++) {
-		std::cout << ((k[i] ^ ke[i])) << std::endl;
-	}
-	cout << int('K') << endl;
+	//Product_cipher(message[5], ProductKey);
+	//Vernam_cipher(newkey,"CON");
+	
 	return 0;
 }
